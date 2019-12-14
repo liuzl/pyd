69279fc38ff7a980594d0f390bade49d122314e7
liuzhanliang <zhanliangliu@qq.com>
2019-12-14 22:19:55 +0800

    pip install pydict-cedar

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydict-cedar",
+    version="0.0.1",
+    author="Zhanliang Liu",
+    author_email="liang@zliu.org",
+    description="An updateable dict service for Python3, multi-pattern match",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liuzl/pyd",
+    packages=setuptools.find_packages(),
+    py_modules=['pydict'],
+    install_requires=[
+        "lsm-db",
+        "pycedar"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
