a6f040e82c87015d1978373238e888418a624821
liuzhanliang <zhanliangliu@qq.com>
2019-12-14 21:09:41 +0800

    get set delete

--- dict.py
+++ dict.py
@@ -1,9 +1,10 @@
 import os
+import pickle
 import lsm
 import pycedar
 
 class Dict(object):
-    def __init__(self, path):
+    def __init__(self, path: str):
         if not os.path.exists(path): os.makedirs(path)
         self.path = path
         self.kv_path = os.path.join(self.path, "kv.ldb")
@@ -12,32 +13,50 @@ class Dict(object):
         self.cedar = pycedar.dict()
         self.cedar.load(self.cedar_path)    
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str):
         return self.get(key)
+    def __setitem__(self, key: str, values: dict):
+        self.replace(key, values)
+    def __delitem__(self, key: str):
+        self.delete(key)
 
-    def get(self, key, default=None):
+
+    def get(self, key: str, default=None):
         try:
-            return self.kv[key]
+            v = self.kv[key]
+            return pickle.loads(v)
         except KeyError as e:
             return default
 
-    def __delitem__(self, key):
-        self.delete(key)
-
-    def delete(self, key):
+    def delete(self, key: str):
         del self.kv[key]
         del self.cedar[key]
 
-    def update(self, key, values):
-        pass
-    def replace(self, key, values):
-        pass
+    def update(self, key: str, values: dict):
+        key = key.strip()
+        if key == "": raise KeyError("Empty key")
+        if values is None or len(values) == 0:
+            raise ValueError("Empty value")
+        old = self.get(key)
+        if old is None:
+            raise KeyError("Key {} not found".format(key))
+        for k, v in values.items():
+            old[k] = v
+        self.replace(key, old)
 
-    def prefix_match(self, text):
+    def replace(self, key: str, values: dict):
+        key = key.strip()
+        if key == "": raise KeyError("Empty key")
+        if values is None or len(values) == 0:
+            raise ValueError("Empty value")
+        self.cedar[key] = len(values)
+        self.kv[key] = pickle.dumps(values)
+
+    def prefix_match(self, text: str):
         pass
-    def multi_match(self, text):
+    def multi_match(self, text: str):
         pass
-    def multi_max_match(self, text):
+    def multi_max_match(self, text: str):
         pass
 
 
@@ -45,5 +64,7 @@ if __name__ == "__main__":
     d = Dict("data")
     value = d.get("中国")
     print(value)
+    d["中国"] = {"China": "中华人民共和国"}
     x = d['中国']
+    print(x)
     del d['中国']
